2c9b5655ab3dffbdd807d504def5727c72c4b264
Richard Hogge <richard@tremolocreative.com>
2020-12-20 11:19:33 -0800

    sun icon spinner; refactor assets and markup; swap in new paragraph web font

---
+++ assets/js/app.js
@@ -0,0 +1,6 @@
+// Obfuscate Email
+const contact = document.getElementById('contact');
+const user = 'richard';
+const site = 'tremolocreative.com';
+
+contact.href = 'mailto:' + user + '@' + site;
