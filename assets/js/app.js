413d633e057ef0c532badca6b0e96d5bf44dfb43
Richard Hogge <richard@tremolocreative.com>
2020-12-20 23:35:44 -0800

    add video background loop when hovering on LA

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,6 +1,23 @@
+// Background Video
+const la = document.getElementById('la');
+const video = document.getElementById('video');
+const about = document.getElementById('about');
+
+la.onmouseover = function() {
+  video.style.opacity = '1';
+  about.style.opacity = '0';
+  // start video from beginning
+}
+
+la.onmouseout = function() {
+  video.style.opacity = '0';
+  about.style.opacity = '.4';
+  // at end of video fade out anyway
+}
+
 // Obfuscate Email
-const contact = document.getElementById('contact');
 const user = 'richard';
 const site = 'tremolocreative.com';
+const contact = document.getElementById('contact');
 
 contact.href = 'mailto:' + user + '@' + site;
